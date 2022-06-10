c4da641b11a5730d4d3c6f06ef0c972b4d084865
Mathieu Calvo <mathieu.calvo@gmail.com>
2022-06-10 12:04:41 +0100

    MVP for search functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(
     __name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    external_stylesheets=[dbc.themes.GRID],
     meta_tags=[{"name": "viewport", "content": "width=device-width"}]
 )
 
